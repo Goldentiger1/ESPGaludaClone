3fd17f1dfc65015858b7445216651aa0646046bb
Joni Berg <joni.berg@opisto.hkro>
2018-04-27 12:26:48 +0300

    Pelaaja voi ampua

---
+++ ESPGALUDA-CLONE/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+	
+		
+	// Update is called once per frame
+	void Update ()
+    {
+        {
+
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                GameObject clone;
+                clone = Instantiate(shot) as GameObject;
+                nextFire = Time.time + fireRate;
+
+                clone.transform.position = shotSpawn.transform.position;
+                //GetComponent<AudioSource>().Play();
+            }
+            if (Input.GetButtonDown("X360_RBumper"))
+            {
+                GameObject clone;
+                clone = Instantiate(shot) as GameObject;
+                nextFire = Time.time + fireRate;
+
+                clone.transform.position = shotSpawn.transform.position;
+                //GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
