a275a393342f617c94cbf3bda0890cacf6c90e56
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-05-02 13:56:34 +0300

    DummyEnemy Skirpti ja prefabi

---
+++ ESPGALUDA-CLONE/Assets/Scripts/DummyEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyEnemy : MonoBehaviour {
+    // TODO: optimize - set this on instantiate, don't search in Awake
+    public Transform playfieldCenter;
+
+    public Vector3 localPos;
+
+	void Awake() {
+        playfieldCenter = GameObject.Find("CameraObject").transform;
+	}
+	
+	void Update () {
+        // stays in place on screen if localPos is not changed
+
+        // testing: circle movement
+        localPos = new Vector3(Mathf.Cos(Time.time), 0f, Mathf.Sin(Time.time)) * 3f;
+
+        transform.position = playfieldCenter.position + localPos;
+	}
+}
