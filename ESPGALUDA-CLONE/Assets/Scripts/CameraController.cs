26457fabf62091474738accdb0d835121d30b02c
Joni Berg <joni.berg@opisto.hkro>
2018-05-02 15:40:43 +0300

    Pari Sriptiä koodia: Ammusten tuhoutuminen ja Pelaajan seuraaminen X Akselilla.

---
+++ ESPGALUDA-CLONE/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        var pos = Vector3.zero; // Nollataan Sijainti.
+        pos.x = player.transform.position.x; // Seurataan pelaajaa (X) Akselilla. 
+       // pos.z = player.transform.position.z; // Seurataan pelaajaa (Z) Akselilla. 
+        transform.localPosition = pos;
+    }
+}
