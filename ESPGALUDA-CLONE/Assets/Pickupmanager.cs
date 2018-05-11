861c4d9c6ec51210a37a9f5760ad3aac1d22dbec
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-05-11 15:16:08 +0300

    Temporary pickup-object created.

---
+++ ESPGALUDA-CLONE/Assets/Pickupmanager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupmanager : MonoBehaviour {
+
+    public string pickupAudioEvent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+        Fabric.EventManager.Instance.PostEvent(pickupAudioEvent);
+    }
+}
