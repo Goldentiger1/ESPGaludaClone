5b8d64010c423e1a73d1a25fb2338bfa87795f39
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-05-22 15:06:50 +0300

    Soldier spawns and moves but not the perfect way needs still to be fixed. Oman skenen muokkaus ja SoldierTriggerPrefab

--- ESPGALUDA-CLONE/Assets/Scripts/SoldierMovement.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/SoldierMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class SoldierMovement : EnemyBehaviour{
 
     public float speed;
-    public List<GameObject> waypoints = new List<GameObject>();
+    //public List<GameObject> waypoints = new List<GameObject>();
+    public Vector3 target;
+
+
 
     void Start(){
         //waypoints = GetComponentInChildren<GameObject>();
@@ -13,5 +16,6 @@ public class SoldierMovement : EnemyBehaviour{
 
     void Update(){
         float movement = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, movement);
     }
 }
--- ESPGALUDA-CLONE/Assets/Scripts/SoldierSpawner.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/SoldierSpawner.cs
@@ -2,6 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoldierSpawner : MonoBehaviour {
+public class SoldierSpawner : MonoBehaviour{
 
+    public Transform spawnPoint;
+    public GameObject soldierEnemy;
+    public Transform[] waypoints;
+
+    float timer;
+    public float tickTime;
+    int currentWaypoint;
+
+    void Start(){
+        waypoints = transform.GetComponentsInChildren<Transform>();
+        spawnPoint = waypoints[1];
+        currentWaypoint = 2;
+    }
+
+    void Update() {
+        if (currentWaypoint < waypoints.Length) {
+            timer += Time.deltaTime;
+            while (timer > tickTime) {
+                var enemy = Instantiate(soldierEnemy, spawnPoint.position, Quaternion.identity);
+                enemy.GetComponent<SoldierMovement>().target = waypoints[currentWaypoint].position;
+                timer -= tickTime;
+                currentWaypoint++;
+            }
+        }
+    }
 }
