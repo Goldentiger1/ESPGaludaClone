27b5e748f3581b89bd0d051cb07b81ffab5a6563
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-05-25 15:33:40 +0300

    Level01-scene brought up to date. Also modified texts and hitpoints for player character. Popcorns now display explosion animation and let out an explosion on death.

--- ESPGALUDA-CLONE/Assets/GameManager.cs
+++ ESPGALUDA-CLONE/Assets/GameManager.cs
@@ -11,10 +11,11 @@ public class GameManager : MonoBehaviour {
     public Text statusText;
     public Text scoreText;
     public Text crystalText;
+    public Text hpText;
 
     public float score;
 
-
+    public GameObject explosion;
     public GameObject crystal;
     public bool kakusei;
 
@@ -29,15 +30,28 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    public void Explosion(int number, Transform enemy) {
+
+        for (int o = 0; o < number; o++) {
+            float radius = Mathf.Log(o + 1, 2); // distance from center
+            float angle = ((o * GOLDEN_RATIO) % 1) * 2 * Mathf.PI; // direction of offset in radians
+            Vector3 offset = new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle));
+            var expl = Instantiate(explosion, enemy.position + offset, Quaternion.Euler(90,0,0));
+            Destroy(expl, 0.5f);
+        }
+    }
+
     public void EnemyKilled(EnemyBehaviour e) {
         score += e.score;
         UpdateLivesScoreText();
     }
 
+
     public void UpdateLivesScoreText() {
         scoreText.text = "Score: " + score;
         statusText.text = "Lives: " + FindObjectOfType<PlayerMovement>().Lives;
         crystalText.text = "Crystals: " + FindObjectOfType<PlayerMovement>().Crystals;
+        hpText.text = "Hitpoints: " + FindObjectOfType<PlayerMovement>().Hitpoints;
 
     }
 
@@ -45,8 +59,10 @@ public class GameManager : MonoBehaviour {
         FindObjectOfType<PlayerMovement>().Lives--;
         UpdateLivesScoreText();
         if (FindObjectOfType<PlayerMovement>().Lives < 0) {
-            statusText.text = "You died";
-            scoreText.text = "sucker!";
+            scoreText.text = "You died!";
+            statusText.text = "Next time";
+            hpText.text = "learn to play";
+            crystalText.text = "You boob!";
         }
     }
 
@@ -64,6 +80,5 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+    }
 }
