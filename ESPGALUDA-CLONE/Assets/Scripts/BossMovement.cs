e6b5e8f18210c383eb321545b40798ea105a46ba
Aaro Leskinen <aaro.leskinen@laajasalonopisto.fi>
2018-05-30 15:45:49 +0300

    Oman skenen muokkaus, BossMovement skriptin muokkaus ja Shot Patternia aloitettu.

--- ESPGALUDA-CLONE/Assets/Scripts/BossMovement.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/BossMovement.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class BossMovement : MonoBehaviour{
 
     public Vector3 localpos;
-    public Vector3 moveTowards;
+    public Transform moveTowards;
+    public float speed;
+
+    void Start()
+    {
+        moveTowards = GameObject.Find("BossWaypoint").GetComponent<Transform>();
+    }
 
     void Update()
     {
+        float movement = speed * Time.deltaTime;
+        localpos = Vector3.MoveTowards(localpos, moveTowards.position, movement);
+        if (Vector3.Distance(transform.position, moveTowards.position) < 0.01f)
+        {
+            speed -= 1;
+        }
         transform.position = World.center.position + localpos;
     }
 
