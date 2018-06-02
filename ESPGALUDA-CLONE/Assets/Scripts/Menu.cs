b5aa47ca613c45838f02a41747f0bc20492a2b88
Joni Berg <berg.joni.petterig@gmail.com>
2018-06-02 16:39:43 +0300

    MenuScene Peliin.

---
+++ ESPGALUDA-CLONE/Assets/Scripts/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas MenuCanvas;
+    public Canvas CreditsCanvas;
+
+
+    void Start()
+    {
+        MenuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
+        CreditsCanvas = GameObject.Find("CreditsCanvas").GetComponent<Canvas>();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01");
+                
+    }
+
+
+    public void CreditsMenu()
+    {
+        MenuCanvas.enabled = false;
+        CreditsCanvas.enabled = true;
+    }
+
+   public void MainMenu()
+    {
+        CreditsCanvas.enabled = false;
+        MenuCanvas.enabled = true;
+    }
+  
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+
+    }
+
+
+
+
+}
