ffd14c94d573902d659e3647574a55e1f25a73b3
Aaro2 <aaro.leskinen@kolumbus.fi>
2018-06-03 23:18:07 +0300

    3.6.018_23:22 -> Oman Skenen muokkaus, BossTrigger scripti.

---
+++ ESPGALUDA-CLONE/Assets/BossTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour {
+    public BossMovement bM;
+
+    void Start()
+    {
+        bM = GameObject.Find("Boss").GetComponent<BossMovement>();
+        bM.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider box)
+    {
+       if(box.tag == "Player")
+        {
+            bM.enabled = true;
+        } 
+    }
+
+}
