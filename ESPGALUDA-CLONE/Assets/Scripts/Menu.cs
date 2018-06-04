a191c3f751e15da54d0f980f5ab9da4ccc14a0e2
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-06-04 10:00:32 +0300

    MenuSceneen pieni muutos.

--- ESPGALUDA-CLONE/Assets/Scripts/Menu.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/Menu.cs
@@ -8,54 +8,41 @@ public class Menu : MonoBehaviour
 {
     public Canvas MenuCanvas;
     public Canvas CreditsCanvas;
-    public Canvas OptionsCanvas;
+   
 
-    void Start()
-    {
+   void Start()
+   {
         MenuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
         CreditsCanvas = GameObject.Find("CreditsCanvas").GetComponent<Canvas>();
-        OptionsCanvas = GameObject.Find("OptionsCanvas").GetComponent<Canvas>();
+       
+   }
 
-    }
-
-    public void PlayGame()
-    {
+   public void PlayGame()
+   {
         SceneManager.LoadScene("Level01");
                 
-    }
+   }
 
 
-    public void CreditsMenu()
-    {
+   public void CreditsMenu()
+   {
         CreditsCanvas.enabled = true;
         MenuCanvas.enabled = false;
-        OptionsCanvas.enabled = false;
-    }
+       
+   }
 
    public void MainMenu()
-    {
+   {
         MenuCanvas.enabled = true;
         CreditsCanvas.enabled = false;
-        OptionsCanvas.enabled = false;
+       
+   }    
 
-    }
-    public void OptionsMenu()
-    {
-        OptionsCanvas.enabled = true;
-        CreditsCanvas.enabled = false;
-        MenuCanvas.enabled = false;
-    }
-
-
-    public void Quit()
-    {
+   public void Quit()
+   {
 
         Application.Quit();
 
 
-    }
-
-
-
-
+   }
 }
