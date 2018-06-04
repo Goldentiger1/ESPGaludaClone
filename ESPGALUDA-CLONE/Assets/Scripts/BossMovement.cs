25071f93cd3ca402acf2e75e3758907b74b856a6
Aaro Leskinen <aaro.leskinen@laajasalonopisto.fi>
2018-06-04 17:32:17 +0300

    BossiMovement scriptti, oma MapLenght prefab poistettu ja Oman skenen muokkaus.

--- ESPGALUDA-CLONE/Assets/Scripts/BossMovement.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/BossMovement.cs
@@ -2,28 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMovement : MonoBehaviour{
-
+public class BossMovement : MonoBehaviour
+{
     public Vector3 localpos;
     public Transform moveTowards;
     public float speed;
-   
+    public Vector3 bossWaypoint;
 
     void Start()
     {
-        moveTowards = GameObject.Find("Waypoint01").GetComponent<Transform>();
         localpos = transform.position - World.center.position;
+        moveTowards = GameObject.Find("BossWaypoint").GetComponent<Transform>();
     }
 
     void Update()
     {
-
+        bossWaypoint = moveTowards.position - World.center.position;
         float movement = speed * Time.deltaTime;
-        localpos = Vector3.MoveTowards(localpos, moveTowards.position, movement);
-        if (Vector3.Distance(transform.position, moveTowards.position) < 0.01f)
-        {
-            speed -= 1;
-        }
+        localpos = Vector3.MoveTowards(localpos, bossWaypoint, movement);
         transform.position = World.center.position + localpos;
     }
 
