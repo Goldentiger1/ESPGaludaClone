1a536f8b46e6dc38083fa62b49937ae5bdfc1086
Aaro Leskinen <aaro.leskinen@laajasalonopisto.fi>
2018-06-05 13:50:51 +0300

    GameManagerin Canvakseen lisäsin BossHealthin, jonkin lainen bossin liikkuminen EI VIELÄ VALMIS, playerillä myös bossinwaypointti

--- ESPGALUDA-CLONE/Assets/Scripts/BossTrigger.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/BossTrigger.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class BossTrigger : MonoBehaviour {
     public BossSpawner bM;
+    public CameraMovement cM;
+    public GameObject bH;
 
     void Start()
     {
         bM = GameObject.Find("BossSpawner").GetComponent<BossSpawner>();
+        cM = GameObject.Find("CameraObject").GetComponent<CameraMovement>();
+        bH = GameObject.Find("BossHealthUI");
+        bH.SetActive(false);
         bM.enabled = false;
+
     }
 
     void OnTriggerEnter(Collider box)
     {
        if(box.tag == "Player")
         {
+            bH.SetActive(true);
             bM.enabled = true;
+            cM.enabled = false;
         } 
     }
 
--- ESPGALUDA-CLONE/Assets/Scripts/BulletsPatternsScript.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/BulletsPatternsScript.cs
@@ -2,45 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum enemyTypes{
+public class BulletsPatternsScript : MonoBehaviour
+{
 
-    Nothing,
-    Cannon,
-    Popcorn,
-    Soldier,
-    Gunship,
-    Boss
-}
-
-public class BulletsPatternsScript : MonoBehaviour{
-
-    public enemyTypes Class;
-   // public float bulletsXDirection;
-   // public float bulletsZDirection;
-   // public Vector3 bulletsMovementDirection;
-    public Transform bulletsSpawnpoint0;
-    public Transform bulletsSpawnpoint1;
-    public Transform bulletsSpawnpoint2;
-    public Transform bulletsSpawnpoint3;
-    // public float bulletsAngle;
-    // public float bulletsSpeed;
-    public GameObject enemyBulletPrefab;
-
-    void Update()
-    {
-        /*
-        float movement = Time.deltaTime * bulletsSpeed;
-        bulletsXDirection = bulletsSpawnpoint.position.x + Mathf.Sin(bulletsAngle) * movement;
-        bulletsZDirection = bulletsSpawnpoint.position.z + Mathf.Cos(bulletsAngle) * movement;
-        bulletsMovementDirection = new Vector3(bulletsXDirection, 0f, bulletsZDirection);
-        */
-
-        if (Class == enemyTypes.Cannon)
-        {
-            GameObject clone0 = Instantiate(enemyBulletPrefab, bulletsSpawnpoint0.position, transform.rotation);
-            GameObject clone1 = Instantiate(enemyBulletPrefab, bulletsSpawnpoint1.position, transform.rotation);
-            GameObject clone2 = Instantiate(enemyBulletPrefab, bulletsSpawnpoint2.position, transform.rotation * Quaternion.LookRotation(new Vector3(-1, 0, 2)));
-            GameObject clone3 = Instantiate(enemyBulletPrefab, bulletsSpawnpoint3.position, transform.rotation * Quaternion.LookRotation(new Vector3(1, 0, 2)));
-        }
-    }
 }
