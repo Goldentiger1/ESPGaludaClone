74201e29b2175eeb6c36734845064ced9f74479a
Aaro2 <aaro.leskinen@kolumbus.fi>
2018-06-05 23:51:40 +0300

    BossShooting started with the nameBulletPatternsScript.

--- ESPGALUDA-CLONE/Assets/Scripts/BulletsPatternsScript.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/BulletsPatternsScript.cs
@@ -2,7 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BulletPatterns
+{
+    Nothing,
+    Regular,
+
+}
+
 public class BulletsPatternsScript : MonoBehaviour
 {
+    public BulletPatterns Patterns;
+    public Transform bossPosition;
+    public Transform playerPosition;
+    public bool fired = true;
 
+    void Start()
+    {
+        bossPosition = GetComponent<Transform>();
+        playerPosition = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(bossPosition.position, playerPosition.position) < 8f)
+        {
+
+        }
+    }
 }
