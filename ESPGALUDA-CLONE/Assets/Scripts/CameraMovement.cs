81341d3c0bb470d218967db892950638dc9382b9
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-06-05 16:19:58 +0300

    HUD updated.

--- ESPGALUDA-CLONE/Assets/Scripts/CameraMovement.cs
+++ ESPGALUDA-CLONE/Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-    public float speed;
+    float speed;
+    public float targetSpeed;
+    public float maxChange;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
-
+        speed = Mathf.MoveTowards(speed, targetSpeed, Time.deltaTime * maxChange);
     }
 }
